3f8e0a3207ff46da62d3b315ffc1ab801eeb936c
Thanongsine Chanthakham <thanong984@gmail.com>
2023-05-21 15:39:56 +0700

    Handle set claim token to API

--- auth_client/lib/login.dart
+++ auth_client/lib/login.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:auth_client/OTPVerification.dart';
+import 'package:auth_client/network_config.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoginPage extends StatefulWidget {
   @override
@@ -8,16 +13,71 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _phoneNumberController = TextEditingController();
-  bool _isLoading = false;
+  bool _isLoading = true;
+  final _auth = FirebaseAuth.instance;
 
   @override
   void initState() {
     _phoneNumberController.text = '+8562058888059';
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // This callback will be called after the widget is built
+      // You can safely set the state here
+      if (_auth.currentUser != null) {
+        _auth.currentUser!.getIdToken().then((value) {
+          /**
+         * Call set claims API here
+         */
+          http
+              .post(
+                  Uri.parse(
+                      NetworkConfig.BASE_URL + NetworkConfig.SET_CLAIMS_PATH),
+                  headers: <String, String>{
+                    'Content-Type': 'application/json; charset=UTF-8',
+                  },
+                  body: jsonEncode(<String, String>{
+                    'token': value,
+                  }))
+              .then((response) {
+            print('Response status: ${response.statusCode}');
+            print('Response body: ${response.body}');
+
+            // Get token from firebase
+            _auth.idTokenChanges().listen((User? user) {
+              if (user != null) {
+                user.getIdToken().then((value) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                          HomeScreen(token: value),
+                    ),
+                  );
+                });
+              } else {
+                print('User is currently signed out!');
+                setState(() {
+                  _isLoading = false;
+                });
+              }
+            });
+          }).catchError((error) => print('Error: $error'
+                  ''));
+        });
+      }
+    });
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    if (_isLoading) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
@@ -76,6 +136,9 @@ class _LoginPageState extends State<LoginPage> {
 }
 
 class HomeScreen extends StatelessWidget {
+  final String token;
+  const HomeScreen({super.key, required this.token});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ auth_client/lib/network_config.dart
@@ -0,0 +1,5 @@
+class NetworkConfig {
+  static String BASE_URL = "https://e217-103-43-78-159.ap.ngrok.io";
+
+  static String SET_CLAIMS_PATH = "/setCustomClaims";
+}
